681562b90259bfde0b509d5839cf87e3043f8f82
Eduardo Fernandes <78389579+EduAFernandes@users.noreply.github.com>
2022-05-25 14:54:42 -0300

    Update ussailingscript.py

--- ussailing/ussailingscript.py
+++ ussailing/ussailingscript.py
@@ -4,14 +4,14 @@ import csv
 import re
 import string
 
-
+# make to open and parse website correctly
 url = 'https://www1.ussailing.org/Utilities/ReportViewer.aspx?repPath=RegionalSailingAssociations&stb=f&spp=f;w=1000&h=500'
 agent = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
 req = requests.get(url, headers=agent)
 soup = bs(req.content, features="lxml")
 
-
+# manipulate Beautifulsoup objects
 realist = []
 headers = []
 c = []
@@ -19,7 +19,7 @@ for a in soup.findAll('tr'):
     for b in a.findAll('td'):
         c.append(b.text)
 
-
+# ordering info
 first_p = c.index("Manchester Sailing Association")
 second_p = c.index("http://asdf")
 realist = c[first_p - 1:second_p + 1]
